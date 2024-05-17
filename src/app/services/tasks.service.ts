046a86012621bb286fc71500e687949a9f05d7f6
Maxim Sushkov <max_123_99@mail.ru>
2024-05-18 02:13:57 +1000

    feat: implemented saving tasks to localStorage when loading a page and when adding a new task

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -7,25 +7,39 @@ import { Status } from '../enums/status';
   providedIn: 'root',
 })
 export class TasksService {
-  // данные о задачах
-  tasks: Task[] = [
-    {
-      header: 'Заголовок первой задачи',
-      title: 'Пример',
-      worker: 'Андрей',
-      deadline: new Date(),
-      status: Status.New,
-      priority: Priority.Middle,
-    },
-    {
-      header: 'Заголовок второй задачи',
-      title: 'Пример номер 2',
-      worker: 'Максим',
-      deadline: new Date(1),
-      status: Status.InProgress,
-      priority: Priority.High,
-    },
-  ];
+  private tasks!: Task[];
+  constructor() {
+    this.loadTasks();
+  }
+
+  private loadTasks(): void {
+    const tasksFromStorage = localStorage.getItem('tasks');
+    if (tasksFromStorage) {
+      this.tasks = JSON.parse(tasksFromStorage);
+    } else {
+      // данные о задачах по умолчанию
+      this.tasks = [
+        {
+          header: 'Заголовок первой задачи',
+          title: 'Пример',
+          worker: 'Андрей',
+          deadline: new Date(),
+          status: Status.New,
+          priority: Priority.Middle,
+        },
+        {
+          header: 'Заголовок второй задачи',
+          title: 'Пример номер 2',
+          worker: 'Максим',
+          deadline: new Date(1),
+          status: Status.InProgress,
+          priority: Priority.High,
+        },
+      ];
+      // сохранение задач в localStorage
+      this.saveTasks();
+    }
+  }
 
   // управление данными о задачах
   getTasks(): Task[] {
@@ -33,5 +47,9 @@ export class TasksService {
   }
   addTask(task: Task): void {
     this.tasks.push(task);
+    this.saveTasks();
+  }
+  private saveTasks(): void {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
